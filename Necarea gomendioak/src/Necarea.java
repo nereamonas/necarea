647ebde7970700c1ded527092bd027b03d2bc1a4
karla <karla@10.109.82.9>
2019-03-07 18:20:20 +0100

    18:20 7-3

--- Necarea gomendioak/src/Necarea.java
+++ Necarea gomendioak/src/Necarea.java
@@ -1,4 +1,22 @@
-
 public class Necarea {
-//o
+	//atributuak
+    private static Necarea nireNecarea=null;
+    
+    
+    
+            
+//eraikitzailea
+            
+    private Necarea() {        
+    }
+    
+    public static synchronized Necarea getNecarea() {
+        if (Necarea.nireNecarea==null) {
+        	Necarea.nireNecarea= new  Necarea();
+        }
+        return Necarea.nireNecarea;
+    }
+
+
+
 }
