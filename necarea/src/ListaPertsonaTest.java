d5d34b6e581b3311006e7d8477e735798d77924c
Nerea <nereagce@gmail.com>
2019-03-18 17:18:04 +0100

    18/03  17:17

--- necarea/src/ListaPertsonaTest.java
+++ necarea/src/ListaPertsonaTest.java
@@ -5,23 +5,61 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ListaPertsonaTest {
+	ListaPertsona lp;
+	Pertsona p1,p2,p3;
 
 	@Before
 	public void setUp() throws Exception {
+		lp=ListaPertsona.getListaPertsona();
+		p1=new Pertsona(1111);
+		p2=new Pertsona(2222);
+		p3=new Pertsona(3333);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		lp=null;
+		p1=null;
+		p2=null;
+		p3=null;
 	}
 
 	@Test
 	public void testGetListaPertsona() {
-		fail("Not yet implemented");
+		assertNotNull(lp);
 	}
 
 	@Test
 	public void testBilatuPertsonaIdz() {
-		fail("Not yet implemented");
+		lp.erreseteatu();
+		lp.gehituPertsona(p1);
+		lp.gehituPertsona(p2);
+		lp.gehituPertsona(p3);
+		assertNotNull(lp.bilatuPertsonaIdz(1111));
+		assertNotNull(lp.bilatuPertsonaIdz(2222));
+		assertNotNull(lp.bilatuPertsonaIdz(3333));
+		assertNull(lp.bilatuPertsonaIdz(4444));
 	}
-
-}
+	
+	@Test
+	public void testInprimatu() {
+		System.out.println("Inprimatu test-a:");
+		lp.erreseteatu();
+		lp.gehituPertsona(p1);
+		lp.gehituPertsona(p2);
+		lp.gehituPertsona(p3);
+		lp.inprimatu();
+	}
+	
+	@Test
+	public void testErreseteatu() {
+		lp.gehituPertsona(p1);
+		lp.gehituPertsona(p2);
+		lp.gehituPertsona(p3);
+		System.out.println("Hiru pertsona daude:");
+		lp.inprimatu();
+		System.out.println("Hutsik dago:");
+		lp.erreseteatu();
+		lp.inprimatu();
+	}
+	
--- necarea/src/Pertsona.java
+++ necarea/src/Pertsona.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class Pertsona {
 	//atributuak
@@ -26,5 +27,34 @@ public class Pertsona {
 
 	public void pelikulaGehitu(Pelikula p) {
 		this.ikusitakoPelikulak.gehituPelikula(p);
+	}
+	
+	
+	
+	//Junit-etarako
+	
+	public int zenbatPelikulaIkusi() {
+		return this.ikusitakoPelikulak.luzera();
+	}
+	
+	public void ikusitakoakErreseteatu() {
+		this.ikusitakoPelikulak.erreseteatu();
+	}
+	
+	public void balorazioakInprimatu() {
+		for(int key : HM.keySet()) {
+			System.out.println(key + ": " + HM.get(key));
+		}
+		
+		
+		//System.out.println(HM.keySet().toString() + HM.values());
+	}
+	
+	public boolean idBerdinaDu(int pId) {
+		return pId==this.id;
+	}
+	
+	public void idInprimatu() {
+		System.out.println(this.id);
 	}
 }
