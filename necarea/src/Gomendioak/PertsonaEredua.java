3f08fecf932bb1f6c3babb4782b45e59a572c997
nerea <nerea@10.109.129.80>
2019-03-29 18:21:42 +0100

    29/03 - 18:21 - lo que hemos hehco en la uni

--- necarea/src/Gomendioak/PertsonaEredua.java
+++ necarea/src/Gomendioak/PertsonaEredua.java
@@ -1,5 +1,9 @@
 package Gomendioak;
 
+import Klaseak.EtiketaGuztiak;
+import Klaseak.NecareaPelikulak;
+import Klaseak.Pertsona;
+
 public class PertsonaEredua {
  
 	Float[][] matrizea;
@@ -18,14 +22,65 @@ public class PertsonaEredua {
 	}
 	
 	
-	public void matrizeaSortu(float[][] balorazioMatrize, float[][] etiketaMatrize) {
+	public float[] matrizeaSortu(float[][] balorazioMatrize, float[][] etiketaMatrize, int pertsonaId) {
+		EtiketaGuztiak eg=EtiketaGuztiak.getEtiketaGuztiak();
+		NecareaPelikulak np=NecareaPelikulak.getNecareaPelikulak();
 		//1- coger las pelis que tengn >3.5 de valoración - balorazioMatrize
+		float[][] matrizP=new float[np.luzera()][eg.luzera()];
+		int kontM=0;
 		
+		float[][] matrizPeliEzIkusitak= new float[np.luzera()][eg.luzera()];
+		float[] pertsonarenBalorazio = balorazioMatrize[pertsonaId];
+		
+		//recorres la etiketaMatrize cogiendo las pelis que ha visto la persona y q sean >3.5
+		for (int i=0;i<pertsonarenBalorazio.length;i++) {
+			if (pertsonarenBalorazio[i]>3.5) {
+				matrizP[kontM]=etiketaMatrize[i];
+				kontM++;
+			}
+		}
+
 		//2-Coges la etiketaMatrize, solo de las pelis que + le han gustado y sumas cada etiketa --> de aquí sacas un vector. q es de todas las pelis que ha visto que le gustan, cuantas veces se ha usado cada etiketa.
 		
+		//gehitu
+		float[] gehitura=new float[eg.luzera()];
+		
+		for (int i=0;i<matrizP.length;i++) {
+			for (int j=0;j<matrizP[0].length;i++) {
+				gehitura[j]=gehitura[j]+matrizP[i][j];
+			}
+		}
+		//ya tenemos el vector --> gehitura
+		
+		
+		
+		//hacer una matriz con las pelis que no ha visto:
+		for (int i=0;i<balorazioMatrize.length;i++) {
+			if(pertsonarenBalorazio[i]!=0) {
+				matrizPeliEzIkusitak[i]=balorazioMatrize[i];
+			}
+		}
+		
+		
+		//hacer la formula del cos
+		float[] kosinuaAplikatuta=new float[matrizPeliEzIkusitak.length];
+		for (int i=0;i<matrizPeliEzIkusitak.length;i++) {
+			float[] vectorBat=matrizP[i];
+			float kosinua=kosinuaKalkulatu(gehitura,vectorBat);
+			kosinuaAplikatuta[i]=kosinua;
+		}
+		
+		
+		return kosinuaAplikatuta;
 		//3- el vector ese q has sacado, lo conparas con el de cada pelikula que le ha gustado y las etiketas de esa peli(de la etiketaMatrize) y en cada uno aplicas la formula de kosinu y te da un numero. cuanto mas se acerque al 1 + le ha gustado
 		
 		
+	}
+	
+	public float kosinuaKalkulatu(float[] gehitura, float[] vectorBat) {
+		
+		//hay q rellenarlo jaja es poner solo la formula
+		return (float) 0.0;
 	}
 	
 }
