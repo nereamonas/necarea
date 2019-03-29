6495aa63e675edcf04a228edbc7b98bed94780ed
nerea <nerea@LAPTOP-8JRPGOP1>
2019-03-29 10:41:53 +0100

    10:41 - 29/03 - monas

--- necarea/src/Gomendioak/PertsonaEredua.java
+++ necarea/src/Gomendioak/PertsonaEredua.java
@@ -17,4 +17,15 @@ public class PertsonaEredua {
 			return PertsonaEredua.nirePertsonaEredua;
 	}
 	
+	
+	public void matrizeaSortu(float[][] balorazioMatrize, float[][] etiketaMatrize) {
+		//1- coger las pelis que tengn >3.5 de valoración - balorazioMatrize
+		
+		//2-Coges la etiketaMatrize, solo de las pelis que + le han gustado y sumas cada etiketa --> de aquí sacas un vector. q es de todas las pelis que ha visto que le gustan, cuantas veces se ha usado cada etiketa.
+		
+		//3- el vector ese q has sacado, lo conparas con el de cada pelikula que le ha gustado y las etiketas de esa peli(de la etiketaMatrize) y en cada uno aplicas la formula de kosinu y te da un numero. cuanto mas se acerque al 1 + le ha gustado
+		
+		
+	}
+	
 }
